cfd9a467d513294a297a45d1c11ba9619ac4cd0a
vedantsinggh <vedantsinggh@gmail.com>
2024-02-25 23:47:15 +0530

    updated main file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 int main()
 {
-	char val = 'A';
-	Node node(val);
+	Node* node = new Node;
+	node->append('A')->append('P')->append('P')->append('L')->append('E')->append(END);
+	node->append('A')->append('R')->append('M')->append(END);
+	node->append('A')->append('S')->append('S')->append(END);
+	node->append('S')->append('S')->append('S')->append(END);
+	println("");
+	char* s = "AP";
+	delete node;
 	return 0;
 }
